1537b45985f21c944c36c084f823f3c6e1c04d7b
Guayaba Software <guayaba.software@gmail.com>
2021-03-24 10:14:43 -0600

    ratings faltantes

--- CoolApi/Services/RatingService.cs
+++ CoolApi/Services/RatingService.cs
@@ -13,6 +13,8 @@ namespace CoolApi.Services
             _ratings.Add("r2", new Rating("r2", 4.99));
             _ratings.Add("luke", new Rating("luke", 3.89));
             _ratings.Add("solo", new Rating("solo", 4.57));
+            _ratings.Add("kenobi", new Rating("kenobi", 4.68));
+            _ratings.Add("leia", new Rating("leia", 4.32));
         }
 
         public IRating GetRating(string id)
--- node/heroes-service/heroes.js
+++ node/heroes-service/heroes.js
@@ -13,6 +13,6 @@ heroes.set(...heroEntry('r2', 'R2D2', ['luke', 'kenobi']));
 heroes.set(...heroEntry('solo', 'Han Solo', ['leia', 'luke']));
 heroes.set(...heroEntry('luke', 'Luke Skywalker', ['r2', 'kenobi', 'solo']));
 heroes.set(...heroEntry('kenobi', 'Obi Wan Kenobi', ['luke', 'r2', 'leia']));
-heroes.set(...heroEntry('leia', 'Leia Amidala', ['luke', 'kenobi', 'solo']));
+heroes.set(...heroEntry('leia', 'Leia Organa', ['luke', 'kenobi', 'solo']));
 
 exports.heroes = heroes;
